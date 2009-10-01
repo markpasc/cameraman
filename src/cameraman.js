2a56abbb12da2e29a1a394fa9bbd6809815623c8
Mark Paschal <markpasc@markpasc.org>
2009-09-30 18:38:37 -0700

    New style instantiation Use apply() properly in javascript too Pass along an 'errorSending' callback when we get one

--- src/cameraman.js
+++ src/cameraman.js
@@ -1,6 +1,71 @@
+/*
+ *  Cameraman
+ */
+
+
 var cameraman = null;
 
 function CameraMan(opts) {
+    var _self = this;
+    this.options = opts;
+    this.cameras = {};
+
+    this.createCamera = function (opts) {
+
+        var container = document.getElementById(opts.id);
+        // TODO: what if id is invalid?
+
+        // make the embed
+        var embedattr = {
+            name: opts.id,
+            id: opts.id,
+            src: _self.url + 'cameraman.swf',
+            width: opts.width,
+            height: opts.height,
+            quality: 'high',
+            allowScriptAccess: 'always',
+            bgcolor: opts.bgColor,
+            pluginspage: 'http://www.macromedia.com/go/getflashplayer',
+            type: 'application/x-shockwave-flash',
+            wmode: opts.wmode,
+            allowfullscreen: (opts.allowFullScreen?'true':'false'),
+            flashvars: 'cameraid=' + opts.id
+        };
+
+        if (opts.sendto) {
+            embedattr.flashvars += '&sendto=' + opts.sendto;
+        }
+
+        if (!opts.wmode) {
+            delete embedattr.wmode; // don't write empty attribute
+        }
+
+        if (navigator.userAgent.match(/MSIE/i)) {
+            var movie = document.createElement('div');
+            // TODO: include flashvars
+            var movieHTML = '<object id="'+embedattr.id+'" data="'+embedattr.src+'" type="application/x-shockwave-flash" width="'+embedattr.width+'" height="'+embedattr.height+'"><param name="movie" value="'+embedattr.src+'" /><param name="AllowScriptAccess" value="always" /><param name="quality" value="high" />'+(embedattr.wmode?'<param name="wmode" value="'+embedattr.wmode+'" /> ':'')+'<param name="bgcolor" value="'+embedattr.bgColor+'" /><param name="allowFullScreen" value="'+(embedattr.allowFullScreen?'true':'false')+'" /><!-- --></object>';
+            movie.innerHTML = movieHTML;
+            container.appendChild(movie);
+        }
+        else {
+            var movie = document.createElement('embed');
+            for (attr in embedattr) {
+                if (embedattr.hasOwnProperty(attr)) {
+                    movie.setAttribute(attr, embedattr[attr]);
+                }
+            }
+            container.appendChild(movie);
+        }
+
+        // make the camera object
+        var newcam = new CameraCamera(opts);
+        _self.cameras[newcam.id] = newcam;
+        return newcam;
+    };
+
+}
+
+function CameraCamera(opts) {
     var _self = this;
     this.options = opts;
     this.id = opts.id;
@@ -22,19 +87,24 @@ function CameraMan(opts) {
 
     this._tookPhoto = function () {
         if (_self.options.tookPhoto) {
-            _self.options.tookPhoto.apply(_self);
+            _self.options.tookPhoto.apply(null, [_self]);
         }
     };
     this._droppedPhoto = function () {
         if (_self.options.droppedPhoto) {
-            _self.options.droppedPhoto.apply(_self);
+            _self.options.droppedPhoto.apply(null, [_self]);
         }
     };
     this._sentPhoto = function (url) {
         if (_self.options.sentPhoto) {
-            _self.options.sentPhoto.apply(_self, url);
+            _self.options.sentPhoto.apply(null, [_self, url]);
         }
     };
+    this._errorSending = function (errormsg) {
+        if (_self.options.errorSending) {
+            _self.options.errorSending.apply(null, [_self, errormsg]);
+        }
+    }
 }
 
-cameraman = new CameraMan();
+cameraman = new CameraMan({});
